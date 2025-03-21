8392984fe6a2b1b4d946f3d9a2620069f1ace5be
Giovana <giovanaalves.1404@gmail.com>
2025-03-21 16:34:24 -0300

    Sua mensagem de commit aqui

--- pagina.js
+++ pagina.js
@@ -1,16 +1,20 @@
-let nome = prompt("Qual é o seu nome?");  // Captura o nome do usuário
-let saudacao = document.createElement('h1'); // Cria um elemento <h1>
-saudacao.textContent = `Seja bem-vindo(a) ${nome}`; // Define o conteúdo do <h1>
-document.body.appendChild(saudacao);  // Adiciona o <h1> ao corpo da página
+let nome = prompt("Qual é o seu nome?"); 
+let saudacao = document.createElement('h1'); 
+saudacao.textContent = `Seja bem-vindo(a) ${nome}`; 
+document.body.appendChild(saudacao); 
 saudacao.style.textAlign = 'center';
-saudacao.style.opacity = '0'; // Inicialmente invisível
-saudacao.style.transition = 'opacity 1s ease'; // Define a transição de opacidade para 1 segundo
+saudacao.style.opacity = '0'; 
+saudacao.style.transition = 'opacity 1s ease'; 
 
-// Evento para verificar o scroll da página
+
+window.addEventListener('load', function() {
+    saudacao.style.opacity = '1';  
+});
+
 window.addEventListener('scroll', function() {
-    if (window.scrollY > 0) {  // Quando o usuário rolar mais de 0px para baixo
-        saudacao.style.opacity = '1'; // Torna o <h1> visível
+    if (window.scrollY > 0) {  
+        saudacao.style.opacity = '1'; 
     } else {
-        saudacao.style.opacity = '0'; // Faz o <h1> desaparecer se o scroll for no topo
+        saudacao.style.opacity = '0'; 
     }
 });
